961645887e18bd77b0f63fc5f102c4483ae19501
Dénes Fintha <denes.fintha@gmail.com>
2019-04-03 22:41:54 +0200

    Small style fix.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,9 +1,9 @@
 #include "kernel.h"
 
 #if (UINT32_MAX == UINTPTR_MAX)
-uintptr_t __stack_chk_guard = 0xe2dee396;
+uintptr_t __stack_chk_guard = 0xE2DEE396;
 #else
-uintptr_t __stack_chk_guard = 0x595e9fbd94fda766;
+uintptr_t __stack_chk_guard = 0x595E9FBD94FDA766;
 #endif
 
 __attribute__((noreturn))

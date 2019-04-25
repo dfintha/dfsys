7d75b38d064de4750c6f6464eeb90facc73f69a6
Dénes Fintha <denes.fintha@gmail.com>
2019-04-26 00:34:51 +0200

    Add TSS structures.

--- include/x86.h
+++ include/x86.h
@@ -21,6 +21,80 @@
 #define GDTFLAG_SZ              (1 << 0)
 #endif
 
+/* task state segment */
+#if defined(ARCH_X86)
+typedef struct __tss {
+    uint16_t reserved1;
+    uint16_t link;
+    uint32_t esp0;
+    uint16_t reserved2;
+    uint16_t ss0;
+    uint32_t esp1;
+    uint16_t reserved3;
+    uint16_t ss1;
+    uint32_t esp2;
+    uint16_t reserved4;
+    uint16_t ss2;
+    uint32_t cr3;
+    uint32_t eip;
+    uint32_t eflags;
+    uint32_t eax;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t ebx;
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t esi;
+    uint32_t edi;
+    uint16_t reserved5;
+    uint16_t es;
+    uint16_t reserved6;
+    uint16_t cs;
+    uint16_t reserved7;
+    uint16_t ss;
+    uint16_t reserved8;
+    uint16_t ds;
+    uint16_t reserved9;
+    uint16_t fs;
+    uint16_t reserved10;
+    uint16_t gs;
+    uint16_t reserved11;
+    uint16_t ldtr;
+    uint16_t iopb_offset;
+    uint16_t reserved12;
+} __attribute__((packed)) tss;
+#else
+typedef struct __tss {
+    uint32_t reserved1;
+    uint32_t rsp0_low;
+    uint32_t rsp0_high;
+    uint32_t rsp1_low;
+    uint32_t rsp1_high;
+    uint32_t rsp2_low;
+    uint32_t rsp2_high;
+    uint32_t reserved2;
+    uint32_t reserved3;
+    uint32_t ist1_low;
+    uint32_t ist1_high;
+    uint32_t ist2_low;
+    uint32_t ist2_high;
+    uint32_t ist3_low;
+    uint32_t ist3_high;
+    uint32_t ist4_low;
+    uint32_t ist4_high;
+    uint32_t ist5_low;
+    uint32_t ist5_high;
+    uint32_t ist6_low;
+    uint32_t ist6_high;
+    uint32_t ist7_low;
+    uint32_t ist7_high;
+    uint32_t reserved4;
+    uint32_t reserved5;
+    uint16_t iopb_offset;
+    uint16_t reserved6;
+} __attribute__((packed)) tss;
+#endif
+
 /* gdt register */
 typedef struct __gdtreg {
     uint16_t limit;
